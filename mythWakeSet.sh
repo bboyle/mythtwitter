9d48c86dcd9825234ebedfbb5c2354d033411d67
benjamins.boyle <benjamins.boyle@14ed4c6c-1627-11de-b69a-45dc1c5e4070>
2009-05-12 12:20:55 +0000

    working in mythbuntu

--- mythWakeSet.sh
+++ mythWakeSet.sh
@@ -2,7 +2,6 @@
 
 # $1 is the first argument to the script. It is the time in seconds since 1970
 # this is defined in mythtv-setup with the time_t argument
-
 # http://www.mythtv.org/wiki/ACPI_Wakeup#Integrate_into_mythTV_2
 
 echo 0 > /sys/class/rtc/rtc0/wakealarm      #this clears your alarm
