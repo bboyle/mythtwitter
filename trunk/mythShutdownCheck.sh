30cad2377717f138dd6e51b8bcbb47211dbce30a
benjamins.boyle <benjamins.boyle@14ed4c6c-1627-11de-b69a-45dc1c5e4070>
2011-05-08 02:08:35 +0000

    added comment re required permissions

--- trunk/mythShutdownCheck.sh
+++ trunk/mythShutdownCheck.sh
@@ -7,6 +7,8 @@
 # returns "1" if yes, stopping shutdown
 # returns "0" if ok to shutdown
 #
+# need sudo permissions
+# http://ubuntuforums.org/showthread.php?t=1370585
 
 if last | head | grep -q "pts/.*still logged in"   # check for active *remote* login?
 
